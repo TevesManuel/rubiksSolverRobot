8f57cf721458a90966739cd481af10a035b73707
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-01-06 18:23:43 -0300

    I understand all

--- testKociemba/src/app.py
+++ testKociemba/src/app.py
@@ -1,28 +1,25 @@
 import kociemba
 
-color_to_face = {
-    'white': 'U',
-    'red': 'R',
-    'green': 'F',
-    'yellow': 'D',
-    'orange': 'L',
-    'blue': 'B',
+colorToFace = {
+    'white'  : 'U',
+    'blue'   : 'R',
+    'red'    : 'F',
+    'yellow' : 'D',
+    'green'  : 'L',
+    'orange' : 'B',
 }
 
 colors = {
-    'U': ['green', 'green', 'green', 'red', 'white', 'yellow', 'red', 'white', 'yellow'],
-    'R': ['blue', 'blue', 'red', 'blue', 'blue', 'white', 'blue', 'blue', 'yellow'],
-    'F': ['yellow', 'red', 'orange', 'yellow', 'red', 'orange', 'yellow', 'red', 'orange'],
-    'D': ['orange', 'yellow', 'white', 'orange', 'yellow', 'white', 'blue', 'blue', 'blue'],
-    'L': ['orange', 'green', 'green', 'yellow', 'green', 'green', 'white', 'green', 'green'],
-    'B': ['white', 'white', 'white', 'orange', 'orange', 'orange', 'red', 'red', 'red'],
+    'U': [ 'green'  , 'green'  , 'green'  , 'red'    , 'white'  , 'yellow' , 'red'    , 'white' , 'yellow' ],
+    'R': [ 'blue'   , 'blue'   , 'red'    , 'blue'   , 'blue'   , 'white'  , 'blue'   , 'blue'  , 'yellow' ],
+    'F': [ 'yellow' , 'red'    , 'orange' , 'yellow' , 'red'    , 'orange' , 'yellow' , 'red'   , 'orange' ],
+    'D': [ 'orange' , 'yellow' , 'white'  , 'orange' , 'yellow' , 'white'  , 'blue'   , 'blue'  , 'blue'   ],
+    'L': [ 'orange' , 'green'  , 'green'  , 'yellow' , 'green'  , 'green'  , 'white'  , 'green' , 'green'  ],
+    'B': [ 'white'  , 'white'  , 'white'  , 'orange' , 'orange' , 'orange' , 'red'    , 'red'   , 'red'    ],
 }
 
-# Convertir a estado
-estado = ''.join(color_to_face[color] for cara in ['U', 'R', 'F', 'D', 'L', 'B'] for color in colors[cara])
-print(estado)
+state = ''.join(colorToFace[color] for cara in ['U', 'R', 'F', 'D', 'L', 'B'] for color in colors[cara])
 
-solucion = kociemba.solve(estado)
-
-# Imprimir la solución
-print("Solución:", solucion)
+print("State: ", state)
+solution = kociemba.solve(state)
+print("Solution:", solution)
